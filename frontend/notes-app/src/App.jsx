da00646ce55ca8da9d000e37d6c4582fa6ae4334
Silver-Runner <rishabhm142003@gmail.com>
2024-09-14 21:51:45 +0530

    updated notes app

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login'
-<<<<<<< HEAD
 import Signup from "./pages/SignUp/SignUp";
-=======
-import Signup from './pages/SignUp/SignUp';
->>>>>>> 8f9c9a78d61785723914bdc9c6e098cd7144852e
 const routes = (
   <Router>
     <Routes>
